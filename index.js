cca92414b10630e90131d4b8e1f6d6de69fba1ef
Haseeb <moh.dhaseeb720@gmail.com>
2020-12-03 09:59:40 +0530

    Post reuest

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
+const Joi = require("joi");
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Hello World");
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
 });
 
-app.listen(3000, () => console.log("listening on 3000"));
+app.get("/api/courses/:id", (req, res) => {
+  let course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("The course with the id was not found");
+  res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+
+  const result = Joi.validate(req.body, schema);
+  console.log(result);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on ${port}`));
